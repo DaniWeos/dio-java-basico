31562c0d22d877d8e9894acedc674b4b97ff3393
DaniWeos <danielweoswork@gmail.com>
2023-02-20 16:30:26 -0300

    Commit final sobre variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //java fortemente tipado
+        short numerocurto = 1;
+        int numeroCurto1 = numerocurto;
+        short numeroCurto2 = (short) numeroCurto1; //realizando um casting (convertendo para uma variável menor)
+
+        //variaveis não constantes
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        //variavel constante
+        final double VALOR_DE_PI = 3.14;
     }
 }
